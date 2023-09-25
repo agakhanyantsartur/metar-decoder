3ab6eb2ee2c2b67e6c48838b7b8bdcb9fccf1edb
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-09-25 21:04:21 +0500

    try catch for metarOutput

--- script.ts
+++ script.ts
@@ -117,4 +117,20 @@ decodeBtn?.addEventListener('click', async () => {
     const apiKey = '80889d19955a28e32b25718458ea5f22'; 
     const apiUrl = `http://api.aviationstack.com/v1/airports?icao=${metarCode}&access_key=${apiKey}`;
 
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (metarDecodeOutput && data.data.length > 0) {
+            const airportName = data.data[0].airport_name;
+            metarDecodeOutput.textContent = `Airport: ${airportName}`;
+        } else if (metarDecodeOutput) {
+            metarDecodeOutput.textContent = 'Airport is not found';
+        }
+    } catch (error) {
+        console.error('Some error occured while fetching the data:', error);
+        if (metarDecodeOutput) {
+            metarDecodeOutput.textContent = 'Error';
+        }
+    }
 });
